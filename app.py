0850a69729af6216a3db1e9d1b9d4f47c854024f
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-04-24 09:51:23 -0500

    Adding ERS

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 
 from flask import Flask, request, jsonify, json, render_template
 from config import app, bd
-from Model import Estudiante
+from Model import Estudiante , loggin
 
-@app.route("/loggin", methods=['POST'])
-def estudiante():
+@app.route("/estudiante", methods=['POST'])
+def registrar():
     usuario = request.json['usuario'] 
     contraseña = request.json['contraseña']
-    entrada = request.json['contraseña']
-    salidad = request.json['salidad']
     newuser = Estudiante(usuario, contraseña)
     bd.session.add(newuser)
     bd.session.commit()     
     return "guardado"
+
+
+@app.route("/loggin", methods=['POST'])
+def loggin():
+    usuario = request.json['usuario'] 
+    contraseña = request.json['contraseña']
+    newuser = loggin(usuario, contraseña)
+    bd.session.add(newuser)
+    bd.session.commit()     
+    return "guardado"
+
